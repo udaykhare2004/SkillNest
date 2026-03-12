9f0115f6adc2b2e48f6f0de123756748ccd98295
Uday <udaykhare1104975@gmail.com>
2026-03-13 03:31:17 +0530

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
